fc921a2bbfb3b78629957468797a114e62418cd1
Athul Titus <titusathul8@gmail.com>
2025-06-07 14:25:33 +0530

    Initial commit

---
+++ qn1.py
@@ -0,0 +1,5 @@
+name = input("What is your name ")
+age = int(input("What is your Age "))
+crnt_year = 2025
+year_100 = crnt_year + (100-age)
+print(f"hello {name} . You will turn 100 years old in the year {year_100}." )
