05a306cf7e3916bf2dae17875bea54f1c5dbb3ad
Athul Titus <titusathul8@gmail.com>
2025-06-24 14:39:27 +0530

    Add qn2.py

---
+++ qn2.py
@@ -0,0 +1,10 @@
+#odd or even
+
+n= int(input("Enter The number :"))
+if n == 0:
+    print("The number is Zero")
+elif n %2 == 0:
+    print("The number is Even")
+else:
+    print("The number is odd")    
+    
